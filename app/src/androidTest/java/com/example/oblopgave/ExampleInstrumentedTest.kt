b6047b266dda5a36c3d0ec6d51f46b3a850088a9
SorenHersomNielsen <hersom@ellekrattet.dk>
2021-11-01 08:21:55 +0100

    UI test

--- app/src/androidTest/java/com/example/oblopgave/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/oblopgave/ExampleInstrumentedTest.kt
@@ -1,12 +1,23 @@
 package com.example.oblopgave
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.clearText
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +26,42 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.oblopgave", appContext.packageName)
+
+        Espresso.onView(withId(R.id.emailInputField))
+            .perform(clearText())
+            .perform(typeText("test@123456.dk"));
+
+        Espresso.onView(withId(R.id.passwordInputField))
+            .perform(clearText())
+            .perform(typeText("123456"))
+
+        Espresso.onView(withId(R.id.sign_in)).perform(ViewActions.click())
+
+        pause(1000);
+
+        
+
+
+        Espresso.onView(ViewMatchers.withText("All Message"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    private fun pause(millis: Long) {
+        try {
+            Thread.sleep(millis)
+            // https://www.repeato.app/android-espresso-why-to-avoid-thread-sleep/
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
